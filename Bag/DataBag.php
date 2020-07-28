6ff4028dc79a39c06a5db1d062194f50730029be
Pedro Miguel <pedro.aos19@gmail.com>
2020-07-28 16:34:27 +0100

    added ArrayAccess to DataBag

--- Bag/DataBag.php
+++ Bag/DataBag.php
@@ -2,7 +2,7 @@
 
 namespace Pantheion\Http\Bag;
 
-class DataBag implements \IteratorAggregate, \Countable
+class DataBag implements \IteratorAggregate, \Countable, \ArrayAccess
 {
     public function __construct(array $data)
     {
@@ -70,5 +70,29 @@ class DataBag implements \IteratorAggregate, \Countable
     public function count()
     {
         return count($this->data);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
 }
